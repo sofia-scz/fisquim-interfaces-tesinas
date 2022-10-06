456f3bf6aa2607ddce97596d1510b7d618747f2e
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-10-06 20:26:43 -0300

    Update plot_posiciones.py

--- ploteo de puntos/CH4-Ni(111)/plot_posiciones.py
+++ ploteo de puntos/CH4-Ni(111)/plot_posiciones.py
@@ -4,68 +4,73 @@ import matplotlib.font_manager as fm
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator,
                                FormatStrFormatter)
 
+# plt set up
 fm = fm.fontManager.addfont(path='Ubuntu-Regular.ttf')
 plt.rc('font', family='Ubuntu', size='14')
 plt.rc('lines', linewidth=1)
-colors = ['#395', '#e63', '#38e', '#c33', '#eb2', '#865']
+colors = ['#c33', '#395', '#38e', '#eb2', '#865', '#a7a', '#e63']
 
-# data
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#            DATA  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+a = 7.5431893271500003/6
+h = 6.5325935828699997/3
 
-x_points = np.array([3.77168, 6.28609, 5.0289, 2.5144, 1.25718, 0,
-                     1.25723, 3.77164, 2.51442])
+# ni points
+# layer 1
+x1_ni = a*np.array([0, 1, 1, 2, 2, 3, 3, 4, 5])
+y1_ni = h*np.array([2, 1, 3, 0, 2, 1, 3, 2, 1])
+# layer 2
+x2_ni = x1_ni.copy()
+y2_ni = y1_ni.copy() - 2*h/3
 
-y_points = np.array([6.53265, 2.17745, 4.3551, 0.00002, 2.17739, 4.35506,
-                    6.5326, 2.1774, 4.35509])
+x2_ni[3], y2_ni[3] = -a, h*(2+1/3)
 
+# grid lines
+grid_lines = [([1, 5], [1, 1]),
+              ([0, 4], [2, 2]),
+              ([1, 3], [3, 3]),
 
-# plots
+              ([0, 2], [2, 0]),
+              ([1, 3], [3, 1]),
+              ([3, 5], [3, 1]),
 
+              ([2, 4], [0, 2]),
+              ([1, 3], [1, 3]),
+              ([0, 1], [2, 3]),
+              ]
+
+# site points
+site_points = {'top': (2*a, 2*h),
+               'bridge': (2.5*a, 1.5*h),
+               'fcc': (2*a, 4*h/3),
+               'hcp': (3*a, 5*h/3),
+               'top-bridge': (2.25*a, 1.75*h),
+               'top-fcc': (2*a, 5*h/3),
+               'top-hcp': (2.5*a, h*(1+5/6))}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#       DO PLOTS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=200)
 
-a = 2.5144
-b = 2.17739
+# plot grid
 
-vx, vy = b*np.cos(np.pi/6)*(2/3), b*np.sin(np.pi/6)*(2/3)
-ux, uy = b*np.cos(np.pi/6), b*np.sin(np.pi/6)
+ax.plot([0], [0], lw=.5, color='gray', label='lattice grid')
+for line in grid_lines:
+    x, y = line
+    x, y = a*np.array(x), h*np.array(y)
+    ax.plot(x, y, lw=.5, color='gray')
 
-# lattice grid
-ax.plot([a, 2*a], [0, 2*b], lw=.5, color='gray', label='lattice grid')
-ax.plot([.5*a, 1.5*a], [b, 3*b], lw=.5, color='gray')
-ax.plot([0, .5*a], [2*b, 3*b], lw=.5, color='gray')
+# plot ni atoms
+ax.scatter(x1_ni, y1_ni, color='#888', label='Ni atom layer 1', s=50)
+ax.scatter(x2_ni, y2_ni, color='#ccc', edgecolor='#888',
+           label='Ni atom layer 2', s=50)
 
-ax.plot([a, 0], [0, 2*b], lw=.5, color='gray')
-ax.plot([a*1.5, .5*a], [b, 3*b], lw=.5, color='gray')
-ax.plot([a*2.5, 1.5*a], [b, 3*b], lw=.5, color='gray')
-
-ax.plot([a/2, 2.5*a], [b, b], lw=.5, color='gray')
-ax.plot([0, 2*a], [2*b, 2*b], lw=.5, color='gray')
-ax.plot([a/2, 1.5*a], [3*b, 3*b], lw=.5, color='gray')
-
-# ni atoms
-ax.scatter(x_points, y_points, color='#888', label='Ni atom', s=50)
-
-# alt line
-# ax.plot([a/2, 2*a], [b, 2*b], lw=.5, color='#395')
-
-# points
+# plot site points
 msize = 50
-ax.scatter([a/2], [b], label='top a', marker='x', lw=2, s=msize,
-           color='#c33')
-ax.scatter([a/2+vx], [b+vy], label='hcp a', marker='x', lw=2, s=msize,
-           color='#e63')
-ax.scatter([a/2+ux], [b+uy], label='bridge a', marker='x', lw=2, s=msize,
-           color='#b69')
-ax.scatter([a/2+2*vx], [b+2*vy], label='fcc a', marker='x', lw=2, s=msize,
-           color='#865')
-
-ax.scatter([a/2], [b*3], label='top b', marker='x', lw=2, s=msize,
-           color='#eb2')
-ax.scatter([a/2], [b*(1+4/3)], label='hcp b', marker='x', lw=2, s=msize,
-           color='#395')
-ax.scatter([a/2], [b*2], label='bridge b', marker='x', lw=2, s=msize,
-           color='#3ac')
-ax.scatter([a/2], [b*(1+2/3)], label='fcc b', marker='x', lw=2, s=msize,
-           color='#38e')
+for i, p_key in enumerate(site_points):
+    x, y = site_points[p_key]
+    ax.scatter(x, y, label=p_key, marker='x', lw=2, s=msize,
+               color=colors[i])
 
 ax.set_aspect('equal', 'box')
 ax.legend(loc='upper right', fontsize=6)
@@ -76,7 +81,7 @@ ax.tick_params(which='major', width=.8, length=4)
 ax.tick_params(which='minor', width=.6, length=3)
 ax.grid(color='grey', linestyle='-', linewidth=.25)
 
-fig.supxlabel(r'Posición x [angs]', va='top', y=.05)
-fig.supylabel('Posición y [angs]', x=0)
+fig.supxlabel(r'Posición x [angs]', y=.05)
+fig.supylabel('Posición y [angs]')
 fig.tight_layout(pad=.4)
 plt.show()
