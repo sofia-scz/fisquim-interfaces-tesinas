721eed375d4e374ec44ce6fa3ffe35f356b2a703
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-10-15 11:39:52 -0300

    Update plot_arq_red.py

--- ploteos/arq_red/plot_arq_red.py
+++ ploteos/arq_red/plot_arq_red.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import ConnectionStyle
 import matplotlib.font_manager as fm
 
 # plt set up
@@ -26,20 +27,20 @@ input_x = np.zeros(input_nodes) + a
 input_y = np.linspace(-h2, h2, input_nodes)
 
 # hidden 1
-h1_nodes = 3
-h2 = h/3
+h1_nodes = 6
+h2 = h/1.5
 hidden1_x = np.zeros(h1_nodes) + 2*a
 hidden1_y = np.linspace(-h2, h2, h1_nodes)
 
 # hidden 2
-h2_nodes = 3
-h2 = h/3
+h2_nodes = 6
+h2 = h/1.5
 hidden2_x = np.zeros(h2_nodes) + 3*a
 hidden2_y = np.linspace(-h2, h2, h2_nodes)
 
 # output
 output_x = np.zeros(1) + 4*a
-output_y = np.linspace(-1, 1, 1)
+output_y = np.linspace(0, 1, 1)
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -76,7 +77,7 @@ ax.scatter(cart_x, cart_y, color='#f99', edgecolor='#c33', s=node_size)
 ax.scatter(input_x, input_y, color='#80cdff', edgecolor='#0072bb', s=node_size)
 ax.scatter(hidden1_x, hidden1_y, color='#bbb', edgecolor='#666', s=node_size)
 ax.scatter(hidden2_x, hidden2_y, color='#bbb', edgecolor='#666', s=node_size)
-ax.scatter(output_x, output_y, color='#8f8', edgecolor='#282',
+ax.scatter(output_x, output_y, color='#ad9', edgecolor='#282',
            s=node_size)
 
 # plot division lines
@@ -86,21 +87,21 @@ ax.plot([3*a/2, 3*a/2], [-h+d, h-d], color='#666', lw=1, ls='--')
 ax.plot([7*a/2, 7*a/2], [-h*.5, h*.5], color='#666', lw=1, ls='--')
 
 # text
-ax.text(-.6, h/2, 'Coordenadas\n  cartesianas')
+ax.text(-.6, h*.5, 'Coordenadas\n  cartesianas')
 ax.text(.8, h+2, 'Input\n layer')
-ax.text(2.5, h/2, 'Hidden\n layers')
-ax.text(3.8, h/5-1, 'Output\n  layer')
+ax.text(2.2, h*.8, 'Hidden\n layers')
+ax.text(3.8, h*.25-1, 'Output\n  layer')
 
-# # arrows + text
-ax.annotate('Cambio a\ncoordenadas\ninvariantes', xy=(.5, -5),
-            xytext=(-.23, -10),
-            arrowprops=dict(color='#444', shrink=0.0, lw=.5),
-            )
+arrowprops = {'color': '#444', 'shrink': 0,
+              'lw': 1, 'connectionstyle': ConnectionStyle("angle3", angleA=0,
+                                                          angleB=-100)}
+ax.annotate("", xy=(.5, -5), xytext=(.2, -8.5), arrowprops=arrowprops)
+ax.text(-.7, -11, 'Cambio a\ncoordenadas\ninvariantes')
 
 # ax.axis('off')
 ax.get_xaxis().set_ticks([])
 ax.get_yaxis().set_ticks([])
-ax.set_xlim(-1, 5)
+ax.set_xlim(-.8, 4.5)
 ax.set_ylim(-12, 15)
 
 fig.tight_layout(pad=.2)
