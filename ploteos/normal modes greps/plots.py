33d60366c31c2fd9cb9a09ae3fabb7c179875e79
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-11-03 09:48:22 -0300

    Add files via upload

---
+++ ploteos/normal modes greps/plots.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.font_manager as fm
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator,
+                               FormatStrFormatter)
+fm = fm.fontManager.addfont(path='Ubuntu-Regular.ttf')
+plt.rc('font', family='Ubuntu', size='14')
+plt.rc('lines', linewidth=1)
+colors = ['#e8e', '#95c', '#38e', '#3bb', '#395', '#dc2', '#fa3',
+          '#e63', '#c33', '#865']
+
+
+def file_to_array(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        lines = [float(line[76:87]) for line in lines]
+    return np.array(lines)
+
+
+files = ['poscars/mode'+str(i) for i in range(1, 10)]
+
+fig, ax = plt.subplots(1, 1, dpi=150, figsize=(7, 5))
+for i, f in enumerate(files):
+    arr = file_to_array(f)
+    ax.plot(np.arange(len(arr))*.1, file_to_array(f), color=colors[i],
+            label='mode '+str(i+1), lw=1.4)
+
+ax.legend(loc='upper right', fontsize=8)
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
+ax.tick_params(which='both', direction='in', top=True, right=True)
+ax.tick_params(which='major', width=.8, length=4)
+ax.tick_params(which='minor', width=.6, length=3)
+ax.grid(color='grey', linestyle='-', linewidth=.25)
+ax.set_ylabel(r'Energia cinetica [eV]')
+ax.set_xlabel('Tiempo [fs]')
+
+fig.tight_layout(pad=.4)
+plt.show()
