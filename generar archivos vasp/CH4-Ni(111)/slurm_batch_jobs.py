ed3c2f48a7a9c55cd1cb29c78a40270086a93ca6
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-11-20 19:23:37 -0300

    Delete generar archivos vasp/CH4-Ni(111) directory

--- generar archivos vasp/CH4-Ni(111)/slurm_batch_jobs.py
+++
@@ -1,8 +0,0 @@
-import os
-
-filename = 'submit_jobs.sh'
-with open(filename, 'w') as ff:
-    ff.write('#!/bin/bash\n')
-    subfolders = [f.path for f in os.scandir('.') if f.is_dir()]
-    for sf in subfolders:
-        ff.write(f'(cd {sf} && sbatch submit_vasp.sh)\n')
